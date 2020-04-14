d5023bc25d87b2a01ca35765ab0b2440633a22e0
PengFei <feifei.feixia@163.com>
2020-04-14 16:33:05 +0800

    getSnapShotList

---
+++ Test.py
@@ -0,0 +1,51 @@
+from pip._vendor.msgpack.fallback import xrange
+
+
+def getListSnapshotDepends(packagePath):
+    with open(packagePath, 'r') as packageGradle:
+        packageGradleStr = packageGradle.read()
+    # print(packageGradleStr)
+    leftMarkIndex = [i for i in xrange(len(packageGradleStr)) if packageGradleStr.startswith('[', i)]
+    # print(leftMarkIndex)
+    rightMarkIndex = [i for i in xrange(len(packageGradleStr)) if packageGradleStr.startswith(']', i)]
+    # print(rightMarkIndex)
+
+    if len(leftMarkIndex) != len(rightMarkIndex):
+        raise RuntimeError('len(leftMarkIndex)!=len(rightMarkIndex)')
+
+    listDependsMaps = []
+    for i in range(0, len(leftMarkIndex)):
+        preDepends = packageGradleStr[leftMarkIndex[i] + 1:rightMarkIndex[i]]
+        preDepends = preDepends.strip().replace(',', '')
+        # print("**************************************************")
+        preDepends = preDepends.splitlines()
+        # print(preDepends)
+        for depend in preDepends:
+            if len(depend) > 0:
+                listDependsMaps.append(depend.strip().replace(' ', '').replace('\'', ''))
+
+    listDepends = []
+    for depend in listDependsMaps:
+        key = depend[0:depend.index(':')]
+        value = depend[depend.index(':') + 1:]
+        listDepends.append({key: value})
+
+    listDependsSnap = []
+    for depend in listDepends:
+        for x, y in depend.items():
+            if y.endswith("-SNAPSHOT"):
+                listDependsSnap.append({x: y})
+
+    for snap in listDependsSnap:
+        for x, y in snap.items():
+            print(x, y)
+
+    return listDependsSnap
+
+
+def main():
+    getListSnapshotDepends('/Users/fei/Codes/PYTHON/Package/package.gradle')
+
+
+if __name__ == "__main__":
+    main()
